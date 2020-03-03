a077ea5c7b983487d2f5b774bbfd94cafc241add
Avra <avrajyoti@gmail.com>
2020-03-02 20:45:33 -0500

    changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static('./dist/angular-forms-validation'));
+ngApp.use(express.static('./dist/ng8App'));
 
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/ng8App/index.html'));
 });
 
 ngApp.listen(process.env.PORT || 8080);
